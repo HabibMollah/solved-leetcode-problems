b96ab72d53302d06d4bb827b7ff45c46e3907b80
HabibMollah <habib.prodev@gmail.com>
2024-08-16 22:58:54 +0600

    solve contains duplicate

---
+++ 0029-contains-duplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set(nums)
+        if len(s) < len(nums):
+            return True
+        return False
---
+++ 0029-contains-duplicate.ts
@@ -0,0 +1,5 @@
+function containsDuplicate(nums: number[]): boolean {
+  nums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) if (nums[i] == nums[i + 1]) return true;
+  return false;
+}
